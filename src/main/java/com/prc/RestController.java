a12a14255c6176d301e63d23966dada71d259a7f
vadrayan <vyas2255@gmail.com>
2021-05-11 17:39:37 +0530

    Addfiles

---
+++ src/main/java/com/prc/RestController.java
@@ -0,0 +1,15 @@
+package com.prc;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
---
+++ src/main/java/com/prc/Test.java
@@ -0,0 +1,17 @@
+package com.prc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test {
+	
+	public static void main(String args[])
+	{
+		SpringApplication.run(Test.class, args);
+		
+	}
+	
+	
+
+}
